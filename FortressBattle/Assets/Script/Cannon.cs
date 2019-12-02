fad330d382fe05b5bce18a41f8625fd7f9538681
HarryharricChen <56270842+HarryharricChen@users.noreply.github.com>
2019-12-01 16:35:59 -0800

    New Fortress Battle

---
+++ FortressBattle/Assets/Script/Cannon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public KeyCode fire;
+    public Transform firepoint;
+    public GameObject bullet;
+    public Rigidbody2D structure;
+    public float knockback = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(fire))
+        {
+            shoot();
+            structure.AddForce(new Vector2 (-knockback, 0));
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(bullet, firepoint.position, firepoint.rotation);
+    }
+}
